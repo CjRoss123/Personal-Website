a5ef2b2825b9e00d11c972b722d1d39e781e757a
CjRoss123 <78881656+CjRoss123@users.noreply.github.com>
2023-09-08 23:42:24 -0400

    Add files via upload

---
+++ Project 2/script.js
@@ -0,0 +1,12 @@
+function linkedInPointer() {
+    window.location.href = "https://www.linkedin.com/in/christopher-ross-84040b149/";
+}
+
+function indeedPointer() {
+    window.location.href = "https://profile.indeed.com/?hl=en_US&co=US&from=gnav-jobsearch--indeedmobile&_ga=2.70765840.1066158272.1694220978-271812446.1669845995&_gac=1.219344619.1694221005.CjwKCAjwjOunBhB4EiwA94JWsD2l6pm1NdIJmkin_P96LeZFj7sSXTrNAZZu7oE5984hqDTw1qKWwBoC74kQAvD_BwE";
+}
+
+function gitHubPointer() {
+    window.location.href = "https://github.com/CjRoss123";
+}
+
